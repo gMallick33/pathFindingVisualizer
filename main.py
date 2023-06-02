a5d400be254e037b7a67320469115fac32ace31e
gyani <g.arya.mallick@gmail.com>
2023-06-03 01:36:02 +0530

    implemented dijkstra and made the repo more modular

---
+++ main.py
@@ -0,0 +1,70 @@
+import sys
+
+import pygame
+from function_utils import *
+from grid_constants import *
+from dijkstra_algo import *
+
+
+def main(win, width):
+
+    arg = sys.argv
+    pygame.display.set_caption("Path Finding Algorithm")
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+
+    run = True
+
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                if not start and spot != end:
+                    start = spot
+                    start.make_start()
+
+                elif not end and spot != start:
+                    end = spot
+                    end.make_end()
+
+                elif spot != end and spot != start:
+                    spot.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                spot = grid[row][col]
+                spot.reset()
+                if spot == start:
+                    start = None
+                elif spot == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for spot in row:
+                            spot.update_neighbors(grid)
+                    if arg[1] == "dijkstra":
+                        dijkstra(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                    else:
+                        astar(lambda: draw(win, grid, ROWS, width), grid, start, end)
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = make_grid(ROWS, width)
+
+    pygame.quit()
+
+
+WIN = pygame.display.set_mode((WIDTH, WIDTH))
+main(WIN, WIDTH)
